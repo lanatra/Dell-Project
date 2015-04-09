4d03f97031a65c4452e396ee92242e235b477f91
Honason <honason@gmail.com>
2015-04-10 00:14:21 +0200

    Switch in Servlet, cleanup

--- src/Presentation/PresentationServlet.java
+++ src/Presentation/PresentationServlet.java
@@ -12,80 +12,48 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Created by Lasse on 09-04-2015.
- */
 public class PresentationServlet extends HttpServlet {
 
     protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    //response.setContentType("text/html;charset=UTF-8");
-        //PrintWriter out = response.getWriter();
-        //out.println("SERVLET REACHED");
-
         Controller cont = AssignController(request);
-
-
-        // The action parameter is a hidden field in html/jsp that designates where the servlet should redirect
-
-        String action = "";
+        String action = ""; // The action parameter is a hidden field in html/jsp that designates where the servlet should redirect
         if(request.getParameter("action") != null)
             action = request.getParameter("action");
         // Can use a switch here instead of if / if-else but I didn't have the right version of the JDK (couldn't use strings in a switch statement)
 
 
+        // if logged in
+        if (request.getSession().getAttribute("User") != null) {
+            request.setAttribute("User", request.getSession().getAttribute("User")); // passing user object to request
 
-       /* try {
-            if (action == null) { // DEFAULT PAGE FORWARD
-                request.getRequestDispatcher("index.jsp").forward(request, response);
-            } else if (action.equals("getUser")) {
-
-                String user_id = request.getParameter("user_id");
-
-                User user = cont.getUser(user_id);
-
-                String user_info = user.userToString();
-
-                request.setAttribute("userInfo", user_info);
-
-                request.getRequestDispatcher("index.jsp").forward(request, response);
-
+            switch (action) {
+                case "getUser":
+                    getUser(request, response, cont);
+                    break;
+                /*
+                // It's good to put actions into separate methods like this, it would be mess in little while otherwise.
+                case "anotherAction":
+                    anotherAction(request, response, cont);
+                    break;
+                 */
+                default:
+                    request.getRequestDispatcher("index.jsp").forward(request, response);
             }
-        }
-        catch (NullPointerException e) {
-            response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.println("nullpointer");
-        }*/
 
-        if(request.getSession().getAttribute("User") != null) {
-            request.setAttribute("User", request.getSession().getAttribute("User"));
-            System.out.println("loggedin");
-            if (action.equals("getUser")) {
-                String user_id = request.getParameter("user_id");
-                User user = cont.getUser(user_id);
-                String user_info = user.toString();
-                request.setAttribute("userInfo", user_info);
-                request.getRequestDispatcher("index.jsp").forward(request, response);
-            }
-            request.getRequestDispatcher("index.jsp").forward(request, response);
         } else {
             if(action.equals("login")) {
                 Object user = cont.login(request.getParameter("email"), request.getParameter("password"));
                 if (user != null) {
-                    System.out.println("correct");
                     request.getSession().setAttribute("User", user);
                     request.getRequestDispatcher("index.jsp").forward(request, response);
                 } else {
-                    System.out.println("incorrect");
                     request.setAttribute("message", "Incorrect password");
                 }
             }
-            System.out.println("fallback");
             response.sendRedirect("/login.jsp");
         }
     }
-
 
     private Controller AssignController(HttpServletRequest request) {
 
@@ -106,14 +74,19 @@ public class PresentationServlet extends HttpServlet {
 
     }
 
-
-
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         process(request, response);
+    }
+
+    void getUser (HttpServletRequest request, HttpServletResponse response, Controller cont) throws ServletException, IOException {
+        String user_id = request.getParameter("user_id");
+        User user = cont.getUser(user_id);
+        String user_info = user.toString();
+        request.setAttribute("userInfo", user_info);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
