935903ed3e54bf267acad26c9339fda0a6e32005
Honason <honason@gmail.com>
2015-05-04 14:56:41 +0200

    Minor styles

--- web/js/project-functions.js
+++ web/js/project-functions.js
@@ -1,17 +1,20 @@
 $('button#submitMessage').click(function (event) {
     event.preventDefault();
-    $.ajax({
-        type: "post",
-        url: "/api/postMessage", //this is my servlet
-        data:   {userId: $('input#userId').val(),
+
+    if($('textarea#message').val() !== "") {
+        $.ajax({
+            type: "post",
+            url: "/api/postMessage", //this is my servlet
+            data:   {userId: $('input#userId').val(),
                 projectId: $('input#projectId').val(),
                 companyId: $('input#companyId').val(),
                 body: $('textarea#message').val()},
-        success: function(msg){
-            $('.project-items').append(msg);
-            formatDates();
-            $('textarea#message').val("");
-        }
-    });
+            success: function(msg){
+                $('.project-items').append(msg);
+                formatDates();
+                $('textarea#message').val("");
+            }
+        });
+    }
 });
 
